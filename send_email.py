e8adfe7c60a79b28f8217d5d29f303ed3f9815b3
abhiiscode <abhismega@gmail.com>
2026-03-11 17:58:46 +0530

    Fix contact page

--- send_email.py
+++ send_email.py
@@ -1,7 +1,21 @@
-import streamlit as st
+import smtplib, ssl
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
-st.title("Contact Me")
-st.write("📧 Email: abhismega@gmail.com")
-st.write("💼 LinkedIn: https://www.linkedin.com/in/abhishekthorat-ai/")
-st.write("🐙 GitHub: https://github.com/abhiiscode")
-st.write("🐦 Twitter: https://x.com/Abhistwitts")
+dotenv_path = Path(__file__).parent / "app3_data.env"
+load_dotenv(dotenv_path=dotenv_path)
+
+def send_email_fung(user_message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "pythonwithabhi@gmail.com"
+    password = os.getenv("PASSWORD_for_app3")
+
+    receiver = "abktpass@gmail.com"
+    my_context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, user_message)
